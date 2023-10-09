fe65cd05c7c966fac1667ea166fb343659c5e843
pligina-e <82607076+pligina-e@users.noreply.github.com>
2023-10-09 23:02:24 +0300

    Update CreateEntityTest.java

--- src/test/java/test/service/tests/CreateEntityTest.java
+++ src/test/java/test/service/tests/CreateEntityTest.java
@@ -6,27 +6,39 @@ import io.qameta.allure.SeverityLevel;
 import io.qameta.allure.Story;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import test.service.steps.Steps;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class CreateEntityTest extends BaseTest {
+    Random random = new Random();
+    ArrayList<String> idList = new ArrayList<>();
+    int countEntities = random.nextInt(4) + 2;
 
     @Test
     @Story("POST Request")
     @Severity(SeverityLevel.NORMAL)
-    @Description("Test Description : create a new entity")
+    @Description("Test Description : create a new entities")
     public void postEntity() {
-        String response = requestSpecification
-                .param("title", "entityName")
-                .param("verified", "true")
-            .when()
-                .post("/api/create")
-            .then()
-                .statusCode(200)
-                .extract()
-                .asString();
+        
+        for (int i = 1; i <= countEntities; i++) {
+            idList.add(
+                requestSpecification
+                        .param("title", "entityName" + i)
+                        .param("verified", "true")
+                    .when()
+                        .post(POST_ENDPOINT)
+                    .then()
+                        .statusCode(200)
+                        .extract()
+                        .asString()
+            );
+        }
 
-        Assert.assertEquals(Integer.parseInt(response), steps.getEntity(requestSpecification, response).getId(), "Содержимое поля id не соответствует указанному знач-ю при создании сущности");
-        Assert.assertEquals("entityName", steps.getEntity(requestSpecification, response).getTitle(), "Содержимое поля title не соответствует указанному знач-ю при создании сущности");
-        Assert.assertEquals(true, steps.getEntity(requestSpecification, response).getVerified(), "Содержимое поля verified не соответствует указанному знач-ю при создании сущности");
+        for (String list : idList) {
+            Assert.assertEquals(Integer.parseInt(list), steps.getEntity(requestSpecification, list).getId(), "Содержимое поля id не соответствует указанному знач-ю при создании сущности");
+            Assert.assertTrue(steps.getEntity(requestSpecification, list).getTitle().contains("entityName"), "Содержимое поля title не соответствует указанному знач-ю при создании сущности");
+            Assert.assertEquals(true, steps.getEntity(requestSpecification, list).getVerified(), "Содержимое поля verified не соответствует указанному знач-ю при создании сущности");
+        }
     }
 }
