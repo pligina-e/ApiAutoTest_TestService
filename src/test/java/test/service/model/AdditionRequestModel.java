51687581f581a0c6cc924e1e10ec3038122d36f9
pligina-e <82607076+pligina-e@users.noreply.github.com>
2023-10-11 16:20:09 +0300

    Update AdditionRequestModel.java

--- src/test/java/test/service/model/AdditionRequestModel.java
+++ src/test/java/test/service/model/AdditionRequestModel.java
@@ -8,4 +8,9 @@ import lombok.Getter;
 public class AdditionRequestModel {
     private String additional_info;
     private Integer additional_number;
+
+    public AdditionRequestModel(String additional_info, Integer additional_number) {
+       this.additional_info = additional_info;
+       this.additional_number = additional_number;
+    }
 }
